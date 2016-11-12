baa0b22dd3335515c366fa9be378e6ccab046d3a
Phoenix <holaux@gmail.com>
2016-11-12 19:19:15 +0800

    Initiating the project

---
+++ BeeQuick-Imitation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeeQuick-Imitation
+//
+//  Created by Phoenix on 11/12/16.
+//  Copyright © 2016 Phoenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
